ad6ff2e73a4d3236c10c98c04c8c0a1746bf1697
Skye Bleed <monarrk@aaathats3as.com>
2020-05-12 20:32:49 -0400

    Generate prettier output

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 // Generate a list of random chords
-fun gen_chords(): List<Chord> {
+fun gen_chords(min: Int, max: Int): List<Chord> {
 	val lst: MutableList<Chord> = mutableListOf()
-	for (i in 0..((10..30).random())) {
+	for (i in 0..((min..max).random())) {
 		lst.add(Chord(max = 2, min = 0))
 	}
 	return lst.toList()
@@ -12,16 +12,24 @@ fun semitone_to_hz(t: Int): Double = 440 * Math.pow(Math.pow(2.0, (1.0 / 12.0)),
 
 fun main() {
 	// Generate chords
-	val chords = gen_chords()
+	val chords = gen_chords(10, 30)
+
+	// Collect information about the song
+	var duration = 0
+	chords.forEach {
+		duration = duration + it.get_len()
+	}
+
+	println("[MusicBot] Starting new song with ${chords.count()} chords and a duration of ${ANSI_BLUE}${duration}${ANSI_RESET} msecs")
 
 	for (c in chords) {
-		println("${ANSI_GREEN}new chord:$ANSI_RESET ${c.dump()}")
+		println("${ANSI_GREEN}new ${c.get_notes()[0].name()} chord:$ANSI_RESET ${c.dump()}")
 		val notes = c.get_notes()
 		for (n in notes) {
 			val note = (notes[(0..notes.count() - 1).random()])
 			val st = note.get_base()
 			println("  :: name: ${note.name()}, st: $st, hz: ${semitone_to_hz(st)}")
-			generate_tone(semitone_to_hz(st), c.get_len() / 4, 500)
+			generate_tone(semitone_to_hz(st), c.get_len() / 4.0, 500)
 		}
 	}
 }
